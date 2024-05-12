1bf2b8690ae27382f40c5355a9112814ea374277
fiyosa <fiyosa97@gmail.com>
2024-05-12 15:45:00 +0700

    edit secret 4

--- router/api/auth.api.go
+++ router/api/auth.api.go
@@ -2,12 +2,18 @@ package api
 
 import (
 	"gin/config/secret"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func AuthSecret(c *gin.Context) {
 	c.JSON(200, gin.H{
+		"data-manual": gin.H{
+			"PORT":      os.Getenv("PORT"),
+			"APP_ENV":   os.Getenv("APP_ENV"),
+			"APP_DEBUG": os.Getenv("APP_DEBUG"),
+		},
 		"data": gin.H{
 			"APP_PORT":  secret.PORT,
 			"APP_ENV":   secret.APP_ENV,
